789121f88ae3ceee23072e58e2c08bfeb1897248
irfanali <irfu3844@gmail.com>
2023-10-09 17:48:07 +0530

    Longest Substring Without Repeating Characters

---
+++ 3. Longest Substring Without Repeating Characters.js
@@ -0,0 +1,16 @@
+var lengthOfLongestSubstring = function(s) {
+    let maxLength = 0;
+    let currentSubstring = "";
+
+    for (let char of s) {
+        if (currentSubstring.includes(char)) {
+            currentSubstring = currentSubstring.slice(currentSubstring.indexOf(char) + 1);
+        }
+        currentSubstring += char;
+        maxLength = Math.max(maxLength, currentSubstring.length);
+    }
+
+    return maxLength;  
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'));
