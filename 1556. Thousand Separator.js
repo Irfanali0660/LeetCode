27dbbe00a8fe649336836de94dae5d5bdbf6b3e9
irfanali <irfu3844@gmail.com>
2023-10-11 18:54:01 +0530

    Thousand Separator

---
+++ 1556. Thousand Separator.js
@@ -0,0 +1,16 @@
+var thousandSeparator = function(n) {
+    if (n < 1000) return n.toString();
+    const str = n.toString();
+    let result = '';
+    let nums = 0;
+    for (let i = str.length - 1; i >= 0; i--) {
+        nums++;
+        result = str[i] + result;
+        if (nums % 3 === 0 && i !== 0) {
+            result = '.' + result;
+        }
+    }
+    return result;
+};
+
+console.log(thousandSeparator(292920));
